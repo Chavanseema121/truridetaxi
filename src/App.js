446670468c75a512b84364f81e539c6e2fe62f24
Chavanseema121 <132899296+Chavanseema121@users.noreply.github.com>
2024-07-25 09:59:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import './App.css';
 import Navbar from "./componants/Navbar";
 
 import Home from './sections/Home';
-import Footer from './sections/Footer'
+
 import Contactus from './sections/Contactus';
+
+import Footer from './sections/Footer'
+
 import Testimonials from './sections/Testimonial';
 
 function App() {
