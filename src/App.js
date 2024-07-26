80a4bc4cf31582e3b180b8d4de7141f49ed7b01f
Chavanseema121 <132899296+Chavanseema121@users.noreply.github.com>
2024-07-26 15:25:18 +0530

    commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-
+import React from 'react';
 import './App.css';
-import Navbar from "./componants/Navbar";
-
+import Navbar from "./components/Navbar"; // Correct the folder name "components"
 import Home from './sections/Home';
-
 import Contactus from './sections/Contactus';
-
-import Footer from './sections/Footer'
-
+import Footer from './sections/Footer';
 import Testimonials from './sections/Testimonial';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Testimonials/>
-    <Contactus/>
-    <Footer/>
-    
+      <Navbar/>
+      <Home/>
+      <Testimonials/>
+      <Contactus/>
+      <Footer/>
     </>
   );
 }
