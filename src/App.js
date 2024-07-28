758ae8a0221334be77514c5014fd040f8205feca
Chavanseema121 <132899296+Chavanseema121@users.noreply.github.com>
2024-07-28 12:48:38 +0530

    updated import path

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import './App.css';
-import Navbar from "./components/Navbar"; 
-import Home from './sections/Home';
-import Contactus from './sections/Contactus';
-import Footer from './sections/Footer';
-import Testimonials from './sections/Testimonial';
-import About from "./sections/About"
-import Features from './sections/Features';
+import React from "react";
+import Navbar from "./components/Navbar";
+import About from "./sections/About";
+import Contactus from "./sections/Contactus";
+import Features from "./sections/Features";
+import Footer from "./sections/Footer";
+import Testimonial from "./sections/Testimonial";
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Home/>
-      <Features/>
-      <About/>
-      <Testimonials/>
-      <Contactus/>
-      <Footer/>
-    </>
+    <div className="App">
+      <Navbar />
+      <About />
+      <Contactus />
+      <Features />
+      <Testimonial />
+      <Footer />
+    </div>
   );
 }
 
